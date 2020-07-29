94baff580eed0573be0506510f9bdc2b755c424d
thavi-lim <thavi.lim@externe.systeme-u.fr>
2020-07-29 14:44:30 +0200

    modification du port du server local

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.route('/hello').get(function(req, res) {
   res.send('hello world !'); 
 });
 
+app.route('/').get(function(req, res) { 
+  res.send('welcome to server on 8081'); 
+});
+
 app.listen(8081,function (){
     console.log('le serveur ecoute le port 8081')
 });
